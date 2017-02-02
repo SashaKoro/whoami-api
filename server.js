44bb65f1ff463a6fd67ef5c186b55c1c7033f96a
SashaKoro <balaskorotkov@gmail.com>
2017-02-02 10:10:53 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+
+const port = process.env.PORT || 2000;
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/public/main.html'));
+});
+
+app.get('/whoami', (req, res) => {
+  res.send({
+    software: req.headers['user-agent'],
+    language: req.headers['accept-language'],
+    ipaddress: req.ip,
+  });
+});
+
+
+
+app.listen(port, () => {
+  console.log(`Opening app on port ${port}`);
+});
+
+module.exports = { app };
